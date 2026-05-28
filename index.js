65c916ee070b25fa5e68206d13d80949de95aaca
Vijaykrishna <vk856549@gmail.com>
2026-05-28 12:08:50 +0530

    updaed url

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ app.get("/", async (req, res) => {
 app.use(
   cors({
     origin: process.env.NODE_ENV === "production"
-    ? "https://learn-lab-f-v2-lg2l6cat7-vijay-krishnas-projects.vercel.app/"
-    : "http://localhost:3000",
+    ? "https://learn-lab-f-v2-lg2l6cat7-vijay-krishnas-projects.vercel.app"
+    : "https://learn-lab-f-v2-lg2l6cat7-vijay-krishnas-projects.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
