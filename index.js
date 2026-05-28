aab3b359bbd2b218f4f47df2aa9eb495ae1fc6b9
Vijaykrishna <vk856549@gmail.com>
2026-05-28 12:03:59 +0530

    updaed fronted url

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get("/", async (req, res) => {
 app.use(
   cors({
     origin: process.env.NODE_ENV === "production"
-    ? "http://localhost:3000"
+    ? "https://learn-lab-f-v2-lg2l6cat7-vijay-krishnas-projects.vercel.app/"
     : "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
